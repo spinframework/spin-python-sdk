dabb9908d54175e04ad9f6feb5ecb0a1fdbd6d1d
Vaughn Dice <vdice@akamai.com>
2026-04-01 09:46:39 -0600

    chore(*): update urls, authors, etc
    Signed-off-by: Vaughn Dice <vdice@akamai.com>

--- src/spin_sdk/__init__.py
+++ src/spin_sdk/__init__.py
@@ -1,6 +1,6 @@
 """Spin Python SDK
 
-This is an SDK for creating [Spin](https://github.com/fermyon/spin) apps using Python.
+This is an SDK for creating [Spin](https://github.com/spinframework/spin) apps using Python.
 
-Note that this SDK supercedes an earlier, experimental version, the documentation for which may be found [here](https://fermyon.github.io/spin-python-sdk/v1/index.html).
+Note that this SDK supercedes an earlier, experimental version, the documentation for which may be found [here](https://spinframework.github.io/spin-python-sdk/v1/index.html).
 """
